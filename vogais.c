21d7d37576ea1e48aeeab904322006a87a15f5cf
Wolney Bueno <55039069+wolneyb@users.noreply.github.com>
2019-10-01 20:51:21 -0300

    Update vogais.c

--- vogais.c
+++ vogais.c
@@ -4,7 +4,7 @@ int main(){
 	int z,y=0;
 	scanf("%[^\n]s", x);
 	for(;x[z]!='\0';z++){
-		if(x[z]=='a'||x[z]=='e'||x[z]=='i'||x[z]=='o'||x[z]=='u')
+		if(x[z]=='a'||x[z]=='e'||x[z]=='i'||x[z]=='o'||x[z]=='u'||x[z]=='A'||x[z]=='E'||x[z]=='I'||x[z]=='O'||x[z]=='U')
 		y++;
 		}
 	printf("Numero de vogais: %d",y);	
